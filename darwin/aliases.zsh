2ae2d05f9f5fe3c4c213ef7699f2591a3702fda8
Andrei Amosov <git@drews.pro>
2024-01-16 02:45:27 +0700

    feat(darwin::aliases): Added new aliases, removed obsolete ones
    Added new aliases for homebrew and gallery-dl
    Removed tailscale alias as it is symlinked by homebrew

--- darwin/aliases.zsh
+++ darwin/aliases.zsh
@@ -1,22 +1,22 @@
+alias bubog='brew update && brew outdated --greedy'
 alias cat='bat -p'
 alias cpac="grep antizapret $HOME/pac | tr -d '\n' | clipcopy"
 alias ffmpeg='ffmpeg -hide_banner'
 alias ffplay='ffplay -hide_banner'
 alias ffprobe='ffprobe -hide_banner'
 alias gdl=gallery-dl
-alias gdl.='gdl -d.'
-alias gdlb='gdl --cookies-from-browser brave'
-alias gdls='gdl --cookies-from-browser safari'
-alias icat="kitten icat"
-alias kd='kitty +kitten diff'
+alias gdld='gallery-dl -d.'
+alias gdlD='gallery-dl -D.'
+alias gdlb='gallery-dl --cookies-from-browser brave'
+alias gdls='gallery-dl --cookies-from-browser safari'
+alias icat='kitten icat'
+alias kd='kitten diff'
 alias kgd='git difftool --no-symlinks --dir-diff'
 alias ks='kitten ssh'
 alias ktd='kitten transfer --confirm-paths --transmit-deltas --direction=download'
 alias ktu='kitten transfer --confirm-paths --transmit-deltas --direction=upload'
 alias ls=lsd
-alias open_cache='open_command "/Users/drews/Library/Mobile Documents/com~apple~CloudDocs/.cache"'
 alias rm='rm -I'
-alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 alias watch=viddy
 alias yt-dlp='yt-dlp --downloader aria2c'
 alias yt-dlp_sb='yt-dlp --downloader aria2c --legacy-server-connect'
