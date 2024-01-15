5087bfce9c60ad885e89b8ef0eae1a537ae6db52
Andrei Amosov <git@drews.pro>
2024-01-16 03:12:17 +0700

    feat(linux::aliases): Added new aliases, removed obsolete ones

--- linux/aliases.zsh
+++ linux/aliases.zsh
@@ -5,15 +5,16 @@ else
 	alias ls='lsd --icon=never'
 fi
 alias cat='bat -p'
-alias download-mods='sudo -u blobbot curl -LOJ --output-dir /opt/container-data/minecraft-server/mods'
 alias ffmpeg='ffmpeg -hide_banner'
 alias ffplay='ffplay -hide_banner'
 alias ffprobe='ffprobe -hide_banner'
-alias icat="kitten icat"
-alias rm='rm -I'
+alias icat='kitten icat'
+alias kd='kitten diff'
 alias ktd='kitten transfer --confirm-paths --transmit-deltas --direction=download'
 alias ktu='kitten transfer --confirm-paths --transmit-deltas --direction=upload'
+alias rm='rm -I'
 alias se=sudoedit
 alias sv='sudo vim'
 alias v=vim
+alias watch=viddy
 alias yt-dlp='yt-dlp --downloader aria2c'
