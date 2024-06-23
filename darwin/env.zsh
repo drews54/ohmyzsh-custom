9efffd23657345b717bbe2ced51b7b1865fcf563
Andrei Amosov <git@drews.pro>
2024-06-23 16:40:38 +0700

    refactor(darwin::env): Change OLLAMA_URL to OLLAMA_HOST to use default endpoint

--- darwin/env.zsh
+++ darwin/env.zsh
@@ -4,4 +4,4 @@ export GPG_TTY=$(tty)
 export LC_ALL=ru_RU.UTF-8
 export MONO_GAC_PREFIX="/opt/homebrew"
 export RESEND_API_KEY=$(security find-generic-password -a RESEND_API_KEY -s resend.com -w)
-export OLLAMA_URL=http://100.87.153.38:11434/api
+export OLLAMA_HOST=archlinuxserver-drews:11434
