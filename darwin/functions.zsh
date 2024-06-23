6db57da15de2f1fdf13534c522484622dc913875
Andrei Amosov <git@drews.pro>
2024-06-23 16:25:39 +0700

    feat(darwin::functions): Add function yy to cd to working directory on exiting yazi

--- darwin/functions.zsh
+++ darwin/functions.zsh
@@ -270,3 +270,11 @@ function lsrf {
   done
   if [[ ! ( -v first_result || -v quiet ) ]]; then print "Files list exhausted."; fi
 }
+function yy() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
+}
