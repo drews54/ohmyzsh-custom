19abb5d6a048dcb58c67770b4a616482f837145a
Andrei Amosov <git@drews.pro>
2024-09-27 22:44:55 +0700

    feat(darwin::zshrc): Add HISTIGNORE variable to prevent logging sudo password from stdin

--- darwin/env.zsh
+++ darwin/env.zsh
@@ -5,3 +5,4 @@ export LC_ALL=ru_RU.UTF-8
 export MONO_GAC_PREFIX="/opt/homebrew"
 export RESEND_API_KEY=$(security find-generic-password -a RESEND_API_KEY -s resend.com -w)
 export OLLAMA_HOST=archlinuxserver-drews:11434
+export HISTIGNORE='*sudo -S*'
