9e8204d58a31a21485292753a330b991e638428c
Andrei Amosov <git@drews.pro>
2024-12-14 16:13:58 +0300

    feat(darwin::aliases): Add bml alias to mount the latest borg acrhive at the homedir mountpoint

--- darwin/aliases.zsh
+++ darwin/aliases.zsh
@@ -1,5 +1,6 @@
 alias awg-private-sftp='sshpass -f ~/.ssh/awg-sftp.pwd sftp awg-private-sftp'
 alias awg-public-sftp='sshpass -f ~/.ssh/awg-sftp.pwd sftp awg-public-sftp'
+alias bml='borg mount ::$(borg list --short --last 1) ~/mount'
 alias bubog='brew update && brew outdated --greedy'
 alias bugbcp='bugbc --prune=all'
 alias cat='bat -p'
