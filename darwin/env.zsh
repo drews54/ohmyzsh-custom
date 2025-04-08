d0f59232fb410e148ae3df042403bd8acbfc0a26
Andrei Amosov <git@drews.pro>
2025-04-08 22:08:36 +0300

    fix(darwin::env): Add XDG_DATA_DIRS as required by czkawka_gui

--- darwin/env.zsh
+++ darwin/env.zsh
@@ -5,3 +5,4 @@ export LC_ALL=ru_RU.UTF-8
 export MONO_GAC_PREFIX="/opt/homebrew"
 export RESEND_API_KEY=$(security find-generic-password -a RESEND_API_KEY -s resend.com -w)
 export HISTIGNORE='*sudo -S*'
+XDG_DATA_DIRS+=/opt/homebrew/share
