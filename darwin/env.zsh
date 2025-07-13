b50407a1e365d09b5eef51e607eb3eec307cccdb
Andrei Amosov <git@drews.pro>
2025-07-13 10:06:12 +0700

    feat(darwin::env): Add HOMEBREW_NO_ENV_HINTS var to suppress hints during an upgrade

--- darwin/env.zsh
+++ darwin/env.zsh
@@ -6,3 +6,4 @@ export MONO_GAC_PREFIX="/opt/homebrew"
 export RESEND_API_KEY=$(security find-generic-password -a RESEND_API_KEY -s resend.com -w)
 export HISTIGNORE='*sudo -S*'
 XDG_DATA_DIRS+=/opt/homebrew/share
+export HOMEBREW_NO_ENV_HINTS=true
