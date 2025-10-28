a64a3569fb5ff738c60e78aa603b12fe636f0682
Andrei Amosov <git@drews.pro>
2025-10-29 06:01:24 +0700

    perf(darwin::env): Remove RESEND_API_KEY as it is no longer used

--- darwin/env.zsh
+++ darwin/env.zsh
@@ -3,7 +3,6 @@ export VISUAL=nvim
 export GPG_TTY=$(tty)
 export LC_ALL=ru_RU.UTF-8
 export MONO_GAC_PREFIX="/opt/homebrew"
-export RESEND_API_KEY=$(security find-generic-password -a RESEND_API_KEY -s resend.com -w)
 export HISTIGNORE='*sudo -S*'
 XDG_DATA_DIRS+=/opt/homebrew/share
 export HOMEBREW_NO_ENV_HINTS=true
