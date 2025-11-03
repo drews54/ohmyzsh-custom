1afdf096b06f519fce96df968944e1e17c2a3a43
Andrei Amosov <git@drews.pro>
2025-11-03 12:46:26 +0700

    fix(darwin::env): Fix setting XDG_DATA_DIRS to first check if it is already set then properly export it

--- darwin/env.zsh
+++ darwin/env.zsh
@@ -3,5 +3,5 @@ export GPG_TTY=$(tty)
 export LC_ALL=ru_RU.UTF-8
 export MONO_GAC_PREFIX="/opt/homebrew"
 export HISTIGNORE='*sudo -S*'
-XDG_DATA_DIRS+=/opt/homebrew/share
+[[ -v XDG_DATA_DIRS ]] && export XDG_DATA_DIRS="/opt/homebrew/share:$XDG_DATA_DIRS" || export XDG_DATA_DIRS="/opt/homebrew/share"
 export HOMEBREW_NO_ENV_HINTS=true
